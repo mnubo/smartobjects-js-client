ed6458ff49c4d7db53bab74f8af4f1e3fae9d490
Jeremy Attali <jeremy.attali@gmail.com>
2015-10-15 23:03:15 -0400

    build: able to build with systemjs

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,13 @@ let Builder = require('systemjs-builder');
 gulp.task('dist', (done) => {
     var builder = new Builder();
 
+    builder.config({
+        baseURL: 'out',
+        paths: {
+            '*': '*.js'
+        }
+    });
+
     builder.buildStatic('mnubo', 'dist/mnubo.js', {
         sourceMap: true
     });
