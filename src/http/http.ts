bbb096257d613b8b4d5bebccd161608bcf7ac6fe
Jeremy Attali <jeremy.attali@gmail.com>
2015-10-19 01:05:40 -0400

    http: wrap XMLHttpRequest around Promise

--- src/http/http.ts
+++ src/http/http.ts
@@ -1,5 +1,38 @@
+import {_} from '../utils/underscore';
+import {Request, RequestMethods} from './Request';
+
+function sendRequest(request: Request) {
+    return new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest();
+
+        xhr.open(RequestMethods[request.method], request.url);
+        xhr.addEventListener('load', () => {
+            request = xhr.response;
+            if (xhr.status === 200) {
+                resolve(xhr.response);
+            } else {
+                reject(Error(xhr.statusText));
+            }
+        });
+        xhr.addEventListener('error', () => {
+            reject(Error('Network error'));
+        });
+
+        if (_.isPresent(request.headers)) {
+            request.headers.forEach((val, header) => {
+                xhr.setRequestHeader(header, val);
+            });
+        }
+
+        xhr.send(request.payload());
+    });
+
+}
+
 export class http {
     static post(url: string, body: string): Promise<any> {
-        return new Promise<any>((() => {}, () => {}));
+        let request = new Request(RequestMethods.POST, null, body, url);
+
+        return sendRequest(request);
     }
 }
