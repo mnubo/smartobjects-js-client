d97ac6bee4738499e52737d0ebd27f8276a8cde1
Jeremy Attali <jeremy.attali@gmail.com>
2015-11-20 16:17:08 -0500

    chore(gulp): add build and dist tasks to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 'use strict';
 
 var gulp = require('gulp');
+var rename = require('gulp-rename');
 var typescript = require('gulp-typescript');
 
-var Builder = require('systemjs-builder');
+var tsProject = typescript.createProject('tsconfig.json');
 
 var config = {
   path: {
@@ -16,21 +17,15 @@ gulp.task('watch', function() {
   gulp.watch(config.path.out + '/**/*.js', ['build']);
 });
 
-gulp.task('dist', function() {
-  var builder = new Builder();
-
-  builder.config({
-    baseURL: 'out',
-    paths: {
-      '*': '*.js'
-    }
-  });
-
-  builder.bundle('mnubo', `${config.path.dist}/mnubo.js`, {
-    sourceMap: true
-  });
-
-  builder.buildStatic('mnubo.static', `${config.path.dist}/mnubo.static.js`, {
-    sourceMap: true
-  });
+gulp.task('build', function() {
+  return tsProject
+    .src()
+    .pipe(typescript(tsProject))
+    .js
+    .pipe(rename(function(path) {
+      path.dirname = path.dirname.substring(4); /* src/ */
+    }))
+    .pipe(gulp.dest('dist/'));
 });
+
+gulp.task('dist', ['build'], function() { });
