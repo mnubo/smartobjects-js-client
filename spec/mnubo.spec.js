a629f13858d3c9ce3ee978f35639793398dfbf84
Jeremy Attali <jeremy.attali@gmail.com>
2016-05-01 18:32:25 -0400

    test(mnubo): fix and add unit tests

--- spec/mnubo.spec.js
+++ spec/mnubo.spec.js
@@ -1,8 +1,7 @@
 /* eslint max-len: 0 */
 /* eslint no-invalid-this: 2 */
 
-const _ = require('lodash');
-const mnubo = require('../out/mnubo');
+const mnubo = require('../dist/mnubo');
 
 describe('mnubo', function() {
   it('should exist', function() {
@@ -14,16 +13,17 @@ describe('mnubo', function() {
       expect(mnubo.Client).toBeDefined();
     });
 
-    it('should accept a client id, a client secret and an environement', function() {
-      const env = mnubo.Environments.SANDBOX;
-      const client = new mnubo.Client('id', 'secret', env);
+    it('should accept a client id, a client secret with "sandbox" environement by default', function() {
+      const client = new mnubo.Client({
+        id: 'id',
+        secret: 'secret'
+      });
 
-      const obj = _.pick(client, ['id', 'secret', 'options']);
-
-      expect(obj).toEqual({
+      expect(client.options).toEqual({
         id: 'id',
         secret: 'secret',
-        options: {
+        env: 'sandbox',
+        httpOptions: {
           protocol: 'https',
           hostname: 'rest.sandbox.mnubo.com',
           port: 443
@@ -31,9 +31,43 @@ describe('mnubo', function() {
       });
     });
 
+    it('should accept a client id, a client secret and a env environement', function() {
+      const client = new mnubo.Client({
+        id: 'id',
+        secret: 'secret',
+        env: 'production'
+      });
+
+      expect(client.options).toEqual({
+        id: 'id',
+        secret: 'secret',
+        env: 'production',
+        httpOptions: {
+          protocol: 'https',
+          hostname: 'rest.api.mnubo.com',
+          port: 443
+        }
+      });
+    });
+
+    it('should build owners, objects, events and search namespaces', function() {
+      const client = new mnubo.Client({
+        id: 'id',
+        secret: 'secret'
+      });
+
+      expect(client.owners).toBeDefined();
+      expect(client.objects).toBeDefined();
+      expect(client.events).toBeDefined();
+      expect(client.search).toBeDefined();
+    });
+
     describe('.getAccessToken()', function() {
       beforeEach(function() {
-        this.client = new mnubo.Client('id', 'secret');
+        this.client = new mnubo.Client({
+          id: 'id',
+          secret: 'secret'
+        });
       });
 
       it('should exist', function() {
