ae942053cff20d60f70547f67d061c5a55163367
Jeremy Attali <jattali@mnubo.com>
2016-12-02 15:41:32 -0500

    chore(typescript): upgrade to typescript 2

--- gulpfile.js
+++ gulpfile.js
@@ -6,17 +6,6 @@ var typescript = require('gulp-typescript');
 
 var tsProject = typescript.createProject('tsconfig.json');
 
-var config = {
-  path: {
-    out: 'out',
-    dist: 'dist'
-  }
-};
-
-gulp.task('watch', function() {
-  gulp.watch(config.path.out + '/**/*.js', ['build']);
-});
-
 gulp.task('build', function() {
   return tsProject
     .src()
