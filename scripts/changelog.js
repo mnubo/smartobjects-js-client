212be5c9c7421af130feb8c298563f1a168d211b
Jeremy Attali <jattali@mnubo.com>
2016-12-06 11:50:59 -0500

    chore(changelog): add changelog script

---
+++ scripts/changelog.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const conventionalChangelog = require('conventional-changelog');
+
+conventionalChangelog({
+    preset: 'angular',
+    releaseCount: 1
+}, {
+    // Conventional Changelog Context
+    // We have to manually set version number so it doesn't get prefixed with `v`
+    // See https://github.com/conventional-changelog/conventional-changelog-core/issues/10
+    currentTag: require('../package.json').version
+}).pipe(process.stdout);
