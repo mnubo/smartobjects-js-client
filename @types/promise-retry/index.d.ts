29f7102e51a1388dc81d72f0bb4d42c5b236d18b
David Francoeur <dfrancoeur04@gmail.com>
2017-07-05 13:00:45 -0400

    feat(types): add type definition for promise-retry

---
+++ @types/promise-retry/index.d.ts
@@ -0,0 +1,20 @@
+// Type definitions for promise-retry
+// Project: https://github.com/IndigoUnited/node-promise-retry
+
+declare function promiseRetry(fn: (retry: promiseRetry.Retry, attempt: number) => Promise<any>, opts?: promiseRetry.RetryOptions): Promise<any>
+
+declare namespace promiseRetry {
+  export interface RetryOptions {
+    retries?: number;
+    factor?: number;
+    minTimeout?: number;
+    maxTimeout?: number;
+    randomize?: boolean;
+  }
+
+  export type Retry = () => Promise<any>;
+}
+
+declare module 'promise-retry' {
+  export = promiseRetry;
+}
