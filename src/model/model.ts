2f57f32603f222d25ebc930a394abffe3dbea21b
David Francoeur <dfrancoeur04@gmail.com>
2018-01-22 09:45:30 -0500

    feat(modeler): add the modeler API along with tests

--- src/model/model.ts
+++ src/model/model.ts
@@ -4,6 +4,7 @@ import {Client} from '../mnubo';
 export interface SimpleType {
   highLevelType: string;
 }
+
 export interface ComplexType {
   highLevelType: string;
   containerType: string;
@@ -14,6 +15,22 @@ export interface Timeseries {
   displayName: string;
   description: string;
   type: SimpleType;
+}
+
+export interface SingleTimeseries {
+  key: string;
+  displayName: string;
+  description: string;
+  type: SimpleType;
+  eventTypeKeys: Array<string>;
+}
+
+export interface SingleObjectAttribute {
+  key: string;
+  displayName: string;
+  description: string;
+  type: ComplexType;
+  objectTypeKeys: Array<string>;
 }
 
 export interface ObjectAttribute {
@@ -40,15 +57,32 @@ export interface Sessionizer {
 
 export interface EventType {
   key: string;
+  displayName: string;
   description: string;
   origin: string;
   timeseries: Array<Timeseries>;
 }
 
+export interface SingleEventType {
+  key: string;
+  displayName: string;
+  description: string;
+  origin: string;
+  timeseriesKeys: Array<string>;
+}
+
 export interface ObjectType {
   key: string;
+  displayName: string;
   description: string;
   objectAttributes: Array<ObjectAttribute>;
+}
+
+export interface SingleObjectType {
+  key: string;
+  displayName: string;
+  description: string;
+  objectAttributesKeys: Array<string>;
 }
 
 export interface Orphans {
@@ -64,15 +98,172 @@ export interface DataModel {
   orphans: Orphans;
 }
 
+export interface EntityOps<A> {
+  create(values: Array<A> | A): Promise<void>;
+  update(key: string, update: { displayName: string, description: string }): Promise<void>;
+
+  generateDeployCode(key: string): Promise<string>;
+  applyDeployCode(key: string, code: string): Promise<void>;
+  deploy(key: string): Promise<void>;
+}
+
+export interface TypeOps<A> {
+  create(values: Array<A> | A): Promise<void>;
+  update(key: string, update: A): Promise<void>;
+  delete(key: string): Promise<void>;
+}
+
+export interface ResetOps {
+  generateResetCode(): Promise<string>;
+  applyResetCode(code: string): Promise<void>;
+  reset(): Promise<void>;
+}
+
+export interface SandboxOnlyOps {
+  timeseriesOps: EntityOps<SingleTimeseries>;
+  ownerAttributesOps: EntityOps<OwnerAttribute>;
+  objectAttributesOps: EntityOps<SingleObjectAttribute>;
+
+  objectTypesOps: TypeOps<SingleObjectType>;
+  eventTypesOps: TypeOps<SingleEventType>;
+  resetOps: ResetOps;
+}
+
 const basePath = '/api/v3/model';
 
-export class Model {
-  constructor(private client: Client) {
+class EntityOpsImpl<A> implements EntityOps<A> {
+  constructor(private client: Client, private path: string) { }
 
+  @authenticate
+  create(value: Array<A> | A): Promise<void> {
+    const valueArray: Array<A>  = Array.isArray(value) ? value : [value];
+    return this.client.post(
+      `${basePath}${this.path}`,
+      valueArray
+    );
   }
 
   @authenticate
-  export(query: object): Promise<DataModel> {
+  update(key: string, update: { displayName: string; description: string; }): Promise<void> {
+    return this.client.put(
+      `${basePath}${this.path}/${key}`,
+      update
+    );
+  }
+
+  @authenticate
+  generateDeployCode(key: string): Promise<string> {
+    return this.client.post(
+      `${basePath}${this.path}/${key}/deploy`,
+      null
+    ).then((response: { code: string }) => response.code);
+  }
+
+  @authenticate
+  applyDeployCode(key: string, code: string): Promise<void> {
+    return this.client.post(
+      `${basePath}${this.path}/${key}/deploy/${code}`,
+      null
+    );
+  }
+
+  @authenticate
+  deploy(key: string): Promise<void> {
+    return this.generateDeployCode(key).then(code => this.applyDeployCode(key, code));
+  }
+}
+
+class TypeOpsImpl<A> implements TypeOps<A> {
+  constructor(private client: Client, private path: string) { }
+
+  @authenticate
+  create(value: Array<A> | A): Promise<void> {
+    const valueArray: Array<A>  = Array.isArray(value) ? value : [value];
+    return this.client.post(
+      `${basePath}${this.path}`,
+      valueArray
+    )
+  }
+
+  @authenticate
+  update(key: string, update: A): Promise<void> {
+    return this.client.put(
+      `${basePath}${this.path}/${key}`,
+      update
+    );
+  }
+
+  @authenticate
+  delete(key: string): Promise<void> {
+    return this.client.delete(`${basePath}${this.path}/${key}`);
+  }
+}
+class ResetOpsImpl implements ResetOps {
+  constructor(private client: Client) { }
+
+  @authenticate
+  generateResetCode(): Promise<string> {
+    return this.client.post(
+      `${basePath}/reset`,
+      null
+    ).then((response: { code: string }) => response.code);
+  }
+
+  @authenticate
+  applyResetCode(code: string): Promise<void> {
+    return this.client.post(
+      `${basePath}/reset/${code}`,
+      null
+    );
+  }
+
+  @authenticate
+  reset(): Promise<void> {
+    return this.generateResetCode().then(code => this.applyResetCode(code));
+  }
+}
+
+export class Model {
+  public sandboxOps: SandboxOnlyOps;
+  constructor(private client: Client) {
+    this.sandboxOps = {
+      timeseriesOps: new EntityOpsImpl<Timeseries>(client, "/timeseries"),
+      ownerAttributesOps: new EntityOpsImpl<OwnerAttribute>(client, "/ownerAttributes"),
+      objectAttributesOps: new EntityOpsImpl<ObjectAttribute>(client, "/objectAttributes"),
+
+      objectTypesOps: new TypeOpsImpl<SingleObjectType>(client, "/objectTypes"),
+      eventTypesOps: new TypeOpsImpl<SingleEventType>(client, "/eventTypes"),
+      resetOps: new ResetOpsImpl(client)
+    };
+  }
+
+  @authenticate
+  export(): Promise<DataModel> {
     return this.client.get(basePath + '/export')
+  }
+
+  @authenticate
+  getTimeseries(): Promise<Array<Timeseries>> {
+    return this.client.get(basePath + '/timeseries')
+  }
+
+  @authenticate
+  getOwnerAttributes(): Promise<Array<OwnerAttribute>> {
+    return this.client.get(basePath + '/ownerAttributes')
+  }
+
+  @authenticate
+  getObjectAttributes(): Promise<Array<ObjectAttribute>> {
+    return this.client.get(basePath + '/objectAttributes')
+  }
+
+  @authenticate
+  getObjectTypes(): Promise<Array<ObjectType>> {
+    return this.client.get(basePath + '/objectTypes')
+  }
+
+  @authenticate
+  getEventTypes(): Promise<Array<EventType>> {
+    return this.client.get(basePath + '/eventTypes')
   }
 }
